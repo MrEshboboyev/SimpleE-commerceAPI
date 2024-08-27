526671d3a43782a1cc46cb552b9822c49cf8f5c7
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-27 19:13:09 +0500

    Add JWT Authentication : Web : Program.cs : configure AddAuthentication with JwtBearer; appsettings.json : JwtSettings:Key, Issuer, Audience; Adding Packages : Microsoft.AspNetCore.Authentication.JwtBearer;

--- SimpleE-commerceAPI.Web/Program.cs
+++ SimpleE-commerceAPI.Web/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using SimpleE_commerceAPI.Domain.Entities;
 using SimpleE_commerceAPI.Infrastructure.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +18,28 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
+
+// configure JWT Authentication
+var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtSettings:Key"]);
+var issuer = builder.Configuration["JwtSettings:Issuer"];
+var audience = builder.Configuration["JwtSettings:Audience"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience
+    };
+});
+
 
 var app = builder.Build();
 
