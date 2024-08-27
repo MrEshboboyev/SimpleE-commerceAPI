74ca4c502e1190624d7d8bef491d9c97762b9541
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-27 22:16:56 +0500

    Unit of work interface implemented : Infrastructure : Create Repositories/UnitOfWork.cs : inject Db Context, Save() method implemented;

---
+++ SimpleE-commerceAPI.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using SimpleE_commerceAPI.Application.Common.Interfaces;
+using SimpleE_commerceAPI.Infrastructure.Data;
+
+namespace SimpleE_commerceAPI.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        // inject Db Context
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
