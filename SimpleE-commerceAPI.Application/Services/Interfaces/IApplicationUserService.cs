66a23a480f9981cb6bdb303d1c41c8ce75105b86
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-27 23:19:49 +0500

    Application User Service interface and implemented : Application : Services/Interfaces/IApplicationUser.cs : GetAllUsers(), GetUserById(), GetUserByEmail(), CreateUser(), UpdateUser(), DeleteUser(); Infrastructure : Implementations/ApplicationUser.cs : inherit from IApplicationUser, methods implemented;

---
+++ SimpleE-commerceAPI.Application/Services/Interfaces/IApplicationUserService.cs
@@ -0,0 +1,14 @@
+﻿using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Application.Services.Interfaces
+{
+    public interface IApplicationUserService
+    {
+        IEnumerable<ApplicationUser> GetAllUsers();
+        ApplicationUser GetUserById(string userId);
+        ApplicationUser GetUserByEmail(string email);
+        void CreateUser(ApplicationUser applicationUser);
+        void UpdateUser(ApplicationUser applicationUser);
+        bool DeleteUser(string userId);
+    }
+}
---
+++ SimpleE-commerceAPI.Infrastructure/Implementations/ApplicationUserService.cs
@@ -0,0 +1,62 @@
+﻿using SimpleE_commerceAPI.Application.Common.Interfaces;
+using SimpleE_commerceAPI.Application.Services.Interfaces;
+using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Infrastructure.Implementations
+{
+    public class ApplicationUserService : IApplicationUserService
+    {
+        // inject IUnitOfWork
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ApplicationUserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void CreateUser(ApplicationUser applicationUser)
+        {
+            _unitOfWork.ApplicationUser.Add(applicationUser);
+            _unitOfWork.Save();
+        }
+
+        public bool DeleteUser(string userId)
+        {
+            try
+            {
+                ApplicationUser? objFromDb = _unitOfWork.ApplicationUser.Get(r => r.Id == userId);
+                if (objFromDb != null)
+                {
+                    _unitOfWork.ApplicationUser.Remove(objFromDb);
+                    _unitOfWork.Save();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<ApplicationUser> GetAllUsers()
+        {
+            return _unitOfWork.ApplicationUser.GetAll();
+        }
+
+        public ApplicationUser GetUserById(string userId)
+        {
+            return _unitOfWork.ApplicationUser.Get(r => r.Id == userId);
+        }
+
+        public ApplicationUser GetUserByEmail(string email)
+        {
+            return _unitOfWork.ApplicationUser.Get(r => r.Email == email);
+        }
+
+        public void UpdateUser(ApplicationUser applicationUser)
+        {
+            _unitOfWork.ApplicationUser.Update(applicationUser);
+            _unitOfWork.Save();
+        }
+    }
+}
