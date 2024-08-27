31bd19f4aeb37a53d85f2b7aed2b445669364602
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-27 19:53:24 +0500

    Db Initializer service implemented : Application : Create Common/Utility/SD.cs : static class and Roles const (Role_Admin, Role_Customer); Infrastructure : Create Implementations/DbInitializer.cs : inherit from IDbInitializer, inject Identity Managers and Db, method implemented;

---
+++ SimpleE-commerceAPI.Application/Common/Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace SimpleE_commerceAPI.Application.Common.Utility
+{
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
+    }
+}
---
+++ SimpleE-commerceAPI.Infrastructure/Implementations/DbInitializer.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SimpleE_commerceAPI.Application.Common.Utility;
+using SimpleE_commerceAPI.Application.Services.Interfaces;
+using SimpleE_commerceAPI.Domain.Entities;
+using SimpleE_commerceAPI.Infrastructure.Data;
+
+namespace SimpleE_commerceAPI.Infrastructure.Implementations
+{
+    public class DbInitializer : IDbInitializer
+    {
+        // inject Identity Managers and Db
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                // migrate
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+
+                // is Admin role is not exist, created roles (Customer and Admin)
+                if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                {
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin));
+                    _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer));
+
+                    // create admin user
+                    _userManager.CreateAsync(new ApplicationUser
+                    {
+                        UserName = "admin@example.com",
+                        Email = "admin@example.com",
+                        FullName = "John Doe",
+                        NormalizedUserName = "ADMIN@EXAMPLE.COM",
+                        NormalizedEmail = "ADMIN@EXAMPLE.COM",
+                        PhoneNumber = "1112223333",
+                    }, "Admin*123").GetAwaiter().GetResult();
+
+                    // finding user
+                    ApplicationUser user = _db.Users.FirstOrDefault(
+                        u => u.Email == "admin@example.com"
+                        );
+
+                    _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
