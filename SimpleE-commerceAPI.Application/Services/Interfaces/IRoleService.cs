1f8b3ca5377676d9c1160b1416628db163f3b1c1
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-27 22:51:47 +0500

    Role Service interface : Application : Create Services/Interfaces/IRoleService.cs : GetAllRoles(), GetRoleById(), GetRoleByName(), CreateRole(), UpdateRole(), DeleteRole();

---
+++ SimpleE-commerceAPI.Application/Services/Interfaces/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SimpleE_commerceAPI.Application.Services.Interfaces
+{
+    public interface IRoleService
+    {
+        IEnumerable<IdentityRole> GetAllRoles();
+        IdentityRole GetRoleById(string roleId);
+        IdentityRole GetRoleByName(string roleName);
+        void CreateRole(IdentityRole identityRole);
+        void UpdateRole(IdentityRole identityRole);
+        void DeleteRole(string roleId);
+    }
+}
