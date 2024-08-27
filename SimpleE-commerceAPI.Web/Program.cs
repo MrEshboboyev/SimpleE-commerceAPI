ca46e4c5451c4da134785043b2f214bab7f970d5
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-27 17:21:40 +0500

    Configure identity with IdentityDbContext; Infrastructure : Create Data/ApplicationDbContext.cs : inherit from IdentityDbContext<ApplicationUser>; Adding Packages : Microsoft.EntityFrameworkCore; Adding Project Ref.s : Domain; Web : Program.cs : configure Identity; Adding Project Ref.s : Domain, Infrastructure;

--- SimpleE-commerceAPI.Web/Program.cs
+++ SimpleE-commerceAPI.Web/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using SimpleE_commerceAPI.Domain.Entities;
+using SimpleE_commerceAPI.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// configure Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
