07c29d9e2ff33e012777b931b6f248954c34dc96
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-28 18:55:44 +0500

    Product Service interface : Application : Create Common/Models/CreateProductModel.cs : Name, Description, Price, StockQuantity, Category; UpdateProductModel.cs : ProductId, Name, Description, Price, StockQuantity, Category; Services : Create Interfaces/IProductService.cs : CRUD with models;

---
+++ SimpleE-commerceAPI.Application/Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using SimpleE_commerceAPI.Application.Common.Models;
+using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Application.Services.Interfaces
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAllProductsAsync();
+        Product GetProductByIdAsync(int productId);
+        Product CreateProductAsync(CreateProductModel model);
+        bool UpdateProductAsync(UpdateProductModel model);
+        bool DeleteProductAsync(int productId);
+    }
+}
