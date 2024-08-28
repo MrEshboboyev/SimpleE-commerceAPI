76e0776de9d04c544ebdcbb09429c41a85a5ed20
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-28 21:20:53 +0500

    ShoppingCartItem repo implemented : Infrastructure : Repositories/ShoppingCartItemRepository.cs : inherit from Repository<ShoppingCartItem>, IShoppingCartItemRepository, inject Db, method implemented;

---
+++ SimpleE-commerceAPI.Infrastructure/Repositories/ShoppingCartItemRepository.cs
@@ -0,0 +1,21 @@
+﻿using SimpleE_commerceAPI.Application.Common.Interfaces;
+using SimpleE_commerceAPI.Domain.Entities;
+using SimpleE_commerceAPI.Infrastructure.Data;
+
+namespace SimpleE_commerceAPI.Infrastructure.Repositories
+{
+    public class ShoppingCartItemRepository : Repository<ShoppingCartItem>, IShoppingCartItemRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ShoppingCartItemRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ShoppingCartItem cartItem)
+        {
+            _db.ShoppingCartItems.Update(cartItem);
+        }
+    }
+}
