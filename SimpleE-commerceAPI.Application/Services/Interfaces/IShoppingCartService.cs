9a1bf540f49b8325c44077f2d2e336afd5391823
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-28 22:11:10 +0500

    ShoppingCart Service implementation : Application : Services/Interfaces/IShoppingCartService.cs :  AddItemToCartAsync() return type edited [void -> ShoppingCartItem]; Infrastructure : Create Implementations/ShoppingCartService.cs : inherit from IShoppingCartService, inject IUnitOfWork, methods implemented;

--- SimpleE-commerceAPI.Application/Services/Interfaces/IShoppingCartService.cs
+++ SimpleE-commerceAPI.Application/Services/Interfaces/IShoppingCartService.cs
@@ -6,8 +6,8 @@ namespace SimpleE_commerceAPI.Application.Services.Interfaces
     public interface IShoppingCartService
     {
         Task<ShoppingCart> GetCartAsync(string userId);
-        Task AddItemToCartAsync(string userId, CreateCartItemModel model);
-        Task UpdateItemInCartAsync(string userId, string cartItemId, UpdateCartItemModel model);
-        Task RemoveItemFromCartAsync(string userId, int cartItemId);
+        Task<ShoppingCartItem> AddItemToCartAsync(string userId, CreateCartItemModel model);
+        Task<ShoppingCartItem> UpdateItemInCartAsync(string userId, UpdateCartItemModel model);
+        Task<bool> RemoveItemFromCartAsync(string userId, int cartItemId);
     }
 }
---
+++ SimpleE-commerceAPI.Infrastructure/Implementations/ShoppingCartService.cs
@@ -0,0 +1,123 @@
+﻿using SimpleE_commerceAPI.Application.Common.Interfaces;
+using SimpleE_commerceAPI.Application.Common.Models;
+using SimpleE_commerceAPI.Application.Services.Interfaces;
+using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Infrastructure.Implementations
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        // inject IUnitOfWork
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ShoppingCartItem> AddItemToCartAsync(string userId, CreateCartItemModel model)
+        {
+            try
+            {
+                // if user haven't cart, create it
+                if (!_unitOfWork.Cart.Any(c => c.UserId == userId))
+                {
+                    // get cart by user Id
+                    _unitOfWork.Cart.Add(new ShoppingCart { UserId = userId });
+                }
+
+                // getting cart by userId
+                var cart = _unitOfWork.Cart.Get(c => c.UserId == userId);
+
+                // check this item.product already exist in this cart
+                var existingItem = _unitOfWork.CartItem.Get(item => 
+                    item.ShoppingCartId == cart.ShoppingCartId && item.ProductId == model.ProductId);
+
+                if (existingItem is not null)
+                {
+                    existingItem.Quantity += model.Quantity;
+                    _unitOfWork.CartItem.Update(existingItem);
+                    _unitOfWork.Save();
+                    return existingItem;
+                }
+                else
+                {
+                    var cartItem = new ShoppingCartItem
+                    {
+                        ShoppingCartId = cart.ShoppingCartId,
+                        Quantity = model.Quantity,
+                        Price = model.Price,
+                        ProductId = model.ProductId
+                    };
+                    _unitOfWork.CartItem.Add(cartItem);
+                    _unitOfWork.Save();
+                    return cartItem;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public async Task<ShoppingCart> GetCartAsync(string userId)
+        {
+            if (!_unitOfWork.Cart.Any(c => c.UserId == userId))
+            {
+                _unitOfWork.Cart.Add(new ShoppingCart { UserId = userId });
+            }
+
+            return _unitOfWork.Cart.Get(c => c.UserId == userId, 
+                includeProperties: "ShoppingCartItems");
+        }
+
+        public async Task<bool> RemoveItemFromCartAsync(string userId, int cartItemId)
+        {
+            try
+            {
+                // getting this Cart for this user
+                var cartFromDb = _unitOfWork.Cart.Get(c => c.UserId == userId);
+
+                // getting cart Item for this cartItemId and cart.ShoppingCartId
+                var cartItemFromDb = _unitOfWork.CartItem.Get(item => 
+                    item.ShoppingCartId == cartFromDb.ShoppingCartId && 
+                    item.ShoppingCartItemId == cartItemId);
+
+                _unitOfWork.CartItem.Remove(cartItemFromDb);
+                _unitOfWork.Save();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<ShoppingCartItem> UpdateItemInCartAsync(string userId, UpdateCartItemModel model)
+        {
+            try
+            {
+                // getting this cart by userId
+                var cart = _unitOfWork.Cart.Get(c => c.UserId == userId);
+
+                // getting this cartItem by cart.ShoppingCartId, cartItemId
+                var cartItemFromDb = _unitOfWork.CartItem.Get(item => 
+                    item.ShoppingCartId == cart.ShoppingCartId && 
+                    item.ShoppingCartItemId == model.ShoppingCartItemId);
+
+                // update cartItem
+                cartItemFromDb.ProductId = model.ProductId;
+                cartItemFromDb.Price = model.Price;
+                cartItemFromDb.Quantity = model.Quantity;
+
+                _unitOfWork.CartItem.Update(cartItemFromDb);
+                _unitOfWork.Save();
+                return cartItemFromDb;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
