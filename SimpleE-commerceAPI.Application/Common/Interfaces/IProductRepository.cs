03d87dccfb33278d6c3171972caf4ab5e1d20243
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-28 18:41:39 +0500

    Product repo interface created; Application : Create Common/Interfaces/IProductRepository.cs: inherit from IRepository<Product>;

---
+++ SimpleE-commerceAPI.Application/Common/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Application.Common.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
