a28abe965219cc579a3ba657cb98aacf00ad80b4
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-28 18:44:28 +0500

    Product repo implemented : Infrastructure : Repositories/ProductRepository.cs : inherit from Repository<Product>, IProductRepository, inject Db, method implemented;

---
+++ SimpleE-commerceAPI.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using SimpleE_commerceAPI.Application.Common.Interfaces;
+using SimpleE_commerceAPI.Domain.Entities;
+using SimpleE_commerceAPI.Infrastructure.Data;
+
+namespace SimpleE_commerceAPI.Infrastructure.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
