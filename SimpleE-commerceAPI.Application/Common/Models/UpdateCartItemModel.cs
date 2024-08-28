9b62661996cbcbf3fe1db0e2516c51b31331f4ec
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-28 20:11:34 +0500

    ShoppingCart Service interface : Application : Create Common/Models/CreateCartItemModel.cs : ShoppingCartId, ProductId, Quantity, Price; UpdateCartIteModel.cs : ShoppingCartItemId, ShoppingCartId, ProductId, Quantity, Price; Services : Create Interfaces/IProductService.cs : GetCartAsync(), AddItemToCartAsync(), UpdateItemInCartAsync(), RemoveItemFromCartAsync();

---
+++ SimpleE-commerceAPI.Application/Common/Models/UpdateCartItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleE_commerceAPI.Application.Common.Models
+{
+    public class UpdateCartItemModel
+    {
+        public int ShoppingCartItemId { get; set; }
+        public int ShoppingCartId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ SimpleE-commerceAPI.Application/Services/Interfaces/IShoppingCartService.cs
@@ -0,0 +1,13 @@
+﻿using SimpleE_commerceAPI.Application.Common.Models;
+using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Application.Services.Interfaces
+{
+    public interface IShoppingCartService
+    {
+        Task<ShoppingCart> GetCartAsync(string userId);
+        Task AddItemToCartAsync(string userId, CreateCartItemModel model);
+        Task UpdateItemInCartAsync(string userId, string cartItemId, UpdateCartItemModel model);
+        Task RemoveItemFromCartAsync(string userId, int cartItemId);
+    }
+}
