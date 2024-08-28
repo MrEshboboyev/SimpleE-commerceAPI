db317e6bbaeaf6fb8eb166115842e16924184e16
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-28 19:52:01 +0500

    Shopping Cart repo interface created; Application : Create Common/Interfaces/IShoppingCartRepository.cs: inherit from IRepository<ShoppingCart>, Update() method;

---
+++ SimpleE-commerceAPI.Application/Common/Interfaces/IShoppingRepository.cs
@@ -0,0 +1,9 @@
+﻿using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Application.Common.Interfaces
+{
+    public interface IShoppingRepository : IRepository<ShoppingCart>
+    {
+        void Update(ShoppingCart cart);
+    }
+}
