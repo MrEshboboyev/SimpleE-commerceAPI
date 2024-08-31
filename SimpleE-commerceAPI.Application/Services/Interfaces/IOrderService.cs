b89b833995f726338dab18e788377bd26d345330
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-31 14:57:31 +0500

    Order Service interface created : Application : Create Services/Interfaces/IOrderService.cs : CreateOrderAsync(); Common/Models/OrderRequestModel.cs : UserId,PaymentMethod, PaymentToken, ShippingAddress;

---
+++ SimpleE-commerceAPI.Application/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using SimpleE_commerceAPI.Application.Common.Models;
+using SimpleE_commerceAPI.Domain.Entities;
+
+namespace SimpleE_commerceAPI.Application.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(OrderRequestModel model);
+    }
+}
