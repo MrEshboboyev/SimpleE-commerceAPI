2990b8c2b579529850ce4672ef88072988306ff6
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-31 16:00:04 +0500

    Payment, Order repo implemented : Infrastructure : Repositories/PaymentRepository.cs : inherit from Repository<Payment>, IPaymentRepository, inject Db, method implemented; Repositories/OrderRepository.cs : inherit from Repository<Order>, IOrderRepository, inject Db, method implemented;

---
+++ SimpleE-commerceAPI.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using SimpleE_commerceAPI.Application.Common.Interfaces;
+using SimpleE_commerceAPI.Domain.Entities;
+using SimpleE_commerceAPI.Infrastructure.Data;
+
+namespace SimpleE_commerceAPI.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Order order)
+        {
+            _db.Orders.Update(order);
+        }
+    }
+}
---
+++ SimpleE-commerceAPI.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,21 @@
+﻿using SimpleE_commerceAPI.Application.Common.Interfaces;
+using SimpleE_commerceAPI.Domain.Entities;
+using SimpleE_commerceAPI.Infrastructure.Data;
+
+namespace SimpleE_commerceAPI.Infrastructure.Repositories
+{
+    public class PaymentRepository : Repository<Payment>, IPaymentRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PaymentRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Payment payment)
+        {
+            _db.Payments.Update(payment);
+        }
+    }
+}
